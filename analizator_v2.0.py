8ad07f6a6d875909ff58d79243463bea18895c85
Vadym Tilihuzov <71652431+Real-4339@users.noreply.github.com>
2022-10-17 03:39:59 +0200

    update

--- analizator_v2.0.py
+++ analizator_v2.0.py
@@ -2,32 +2,63 @@
 
 from ruamel.yaml import YAML, scalarstring
 from scapy.all import rdpcap
+from argparse import ArgumentParser
 from pprint import pprint
 from module import *
 
 import os
 
-packets = rdpcap("vzory/trace-26.pcap")
+packets = rdpcap("vzory/trace-15.pcap")
 
 def main():
-	pcap_filename = 'vzory/eth-1.pcap'
+    pcap_filename = 'vzory/trace-7.pcap'
 
     # dictoinary['pcap_name'] = "trace-27.pcap"
 
-	yaml = YAML()
-	file = open("PKS.yaml", "w")
-	yaml.default_flow_style = False
-	outputs = yaml.dump(dictoinary, file)
-	file.close()
+    yaml = YAML()
+    file = open("PKS.yaml", "w")
+    yaml.default_flow_style = False
+    outputs = yaml.dump(dictoinary, file)
+    file.close()
+
+def prepinac(protocol):
+    if protocol == 'TFTP':
+        p = TFTP(protocol, packets)
+        p.parsePackets()
+        p.printAll()
+        
+    else:   
+        p = Prepinac(protocol, packets)
+        p.parsePackets()
+        for flags in eval("Prepinac." + p.low).values():
+            print(p.define(flags))
+        p.printAll()
+
+def statistics():
+    ParseFile()
+    stat = Statistics()
+    start_statistics(stat)
+
+    for index, packet in enumerate(packets):
+        print("<-----Packet----->")
+        p = Packet(packet, index+1)
+        p.printAll()
+        # if index == 10:
+        #     break
+    
+    stat.printStatisctics()
 
 if __name__ == '__main__':
-	
-	stat = Statistics()
-	start_statistics(stat)
+    names = ['HTTP', 'HTTPS', 'TELNET', 'SSH', 'FTP-CONTROL', 'FTP-DATA', 'TFTP', 'ARP']
+    parser = ArgumentParser(description='Input HTTP, HTTPS, TELNET, SSH, FTP, TFTP, ARP: ')
+    parser.add_argument('-p', '--protocol', metavar='protocol', required=False)
+    args = parser.parse_args()
 
-	for index, packet in enumerate(packets):
-		print("<-----Packet----->")
-		p = Packet(packet, index+1)
-		p.printAll()
-
-	stat.printStatisctics()
+    if args.protocol:
+        if args.protocol in names:
+            prepinac(args.protocol)
+        else:
+            print("Error")
+    else:
+        statistics()
+    
