1d890b9256b2eb48cde26a4440b47e2f3ef853f9
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:21:52 +0200

    OK?

---
+++ analyzer/tcp_ip/l1/llc.py
@@ -0,0 +1,33 @@
+import logging
+
+from type import L1, ListOfSAPs
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('LLC')
+
+
+class LLC(L1):
+    name = "IEEE 802.3 LLC"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+        self.__length = self.list_to_str(hex[12:14]).replace(" ", "")
+        self.__type = self.resolve_type(hex[14])
+
+    @property
+    def length(self) -> str:
+        return str(int(self.__length, 16))
+    
+    @property
+    def type(self) -> str | None:
+        return self.__type
+    
+    def print_all(self) -> None:
+        super().print_all()
+        LOGGER.info(f"Length: {self.length}")
+        LOGGER.info(f"Type: {self.type}")
+
+    def resolve_type(self, sap: str) -> str | None:
+        return ListOfSAPs.get(sap)
