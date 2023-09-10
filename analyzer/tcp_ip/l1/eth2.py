faa34edd01af788706bfbf4ff3fa7eb80175dda3
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:28:49 +0200

    OK?

---
+++ analyzer/tcp_ip/l1/eth2.py
@@ -0,0 +1,27 @@
+import logging
+
+from type import L1, ListOfEthernetII
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('EthernetII')
+
+
+class EthernetII(L1):
+    name = "Ethernet II"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+        self.__type = self.resolve_type(hex[12:14])
+
+    @property
+    def type(self) -> str | None:
+        return self.__type
+    
+    def print_all(self) -> None:
+        super().print_all()
+        LOGGER.info(f"Type: {self.type}")
+
+    def resolve_type(self, type: list[str]) -> str | None:
+        return ListOfEthernetII.get(self.list_to_str(type).replace(" ", ""))
