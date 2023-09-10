af3cbe6d80f4f26f30a0fc6f1c7c57ef41886ffe
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:47:29 +0200

    OK

---
+++ analyzer/tcp_ip/l2/ipv6.py
@@ -0,0 +1,18 @@
+import logging
+
+
+from type import L2
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('IPv6')
+
+
+class IPv6(L2):
+    name = "Internet Protocol version 6"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+    def print_all(self) -> None:
+        super().print_all()
