ac59e31b0074421f012220f9421dbf8a43b61fcd
Real-4339 <qvadym@stuba.sk>
2023-09-11 00:40:45 +0200

    created packet class

---
+++ analyzer/tcp_ip/packet.py
@@ -0,0 +1,10 @@
+class Packet:
+    def __init__(self, packet, frame_num: int) -> None:
+        self._hex = self.get_hex(packet)
+        self._frame_num = frame_num
+
+    def get_hex(self, packet) -> list[str]:
+        data = bytes(packet).hex().upper()
+        data = [data[i:i+2] for i in range(0, len(data), 2)]
+
+        return data
