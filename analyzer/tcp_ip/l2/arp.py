e195683f19c82c39474ff5b4acfe7adfd4f568a8
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:55:33 +0200

    OK

---
+++ analyzer/tcp_ip/l2/arp.py
@@ -0,0 +1,36 @@
+import logging
+
+
+from type import L2
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('ARP')
+
+
+class ARP(L2):
+    name = "Address Resolution Protocol"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+        self.__options = {
+            1: "Request",
+            2: "Reply",
+        }
+
+        self.__opcode = self.__options.get(int(self.hex[7], 16))
+
+    @property
+    def opcode(self) -> str:
+        return self.__opcode
+
+    def print_all(self) -> None:
+        super().print_all()
+
+        LOGGER.info(f"Opcode: {self.opcode}")
+        LOGGER.info(f"Sender MAC address: {self.list_to_str(self.hex[8:14]).replace(' ', ':')}")
+        LOGGER.info(f"Target MAC address: {self.list_to_str(self.hex[18:24]).replace(' ', ':')}")
+
+        LOGGER.info(f"Sender IP address: {self.get_ip(self.list_to_str(self.hex[14:18]))}")
+        LOGGER.info(f"Target IP address: {self.get_ip(self.list_to_str(self.hex[24:28]))}")
