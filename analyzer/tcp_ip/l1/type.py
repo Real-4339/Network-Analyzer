4302f085d82480d0bd50b144f8a9958111bdb08b
Real-4339 <qvadym@stuba.sk>
2023-09-11 00:51:36 +0200

    l1 parent class is ready

---
+++ analyzer/tcp_ip/l1/type.py
@@ -0,0 +1,19 @@
+import logging
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('L1')
+
+
+class L1:
+    def __init__(self, name, hex) -> None:
+        self.__name = name
+
+        self.__dst_mac = self.list_to_str(hex[0:6]).replace(" ", ":")
+        self.__src_mac = self.list_to_str(hex[6:12]).replace(" ", ":")
+
+    def list_to_str(self, data: list[str]) -> str:
+        return ''.join(data)
+    
+    def print_all(self) -> None:
+        ...
