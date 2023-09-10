11e75a7527f839800c06f3ff6cebf93f820eeb14
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:01:26 +0200

    updated print func

--- analyzer/tcp_ip/l1/type.py
+++ analyzer/tcp_ip/l1/type.py
@@ -12,11 +12,25 @@ class L1:
         self.__dst_mac = self.list_to_str(hex[0:6]).replace(" ", ":")
         self.__src_mac = self.list_to_str(hex[6:12]).replace(" ", ":")
 
+    @property
+    def name(self) -> str:
+        return self.__name
+    
+    @property
+    def dst_mac(self) -> str:
+        return self.__dst_mac
+    
+    @property
+    def src_mac(self) -> str:
+        return self.__src_mac
+
     def list_to_str(self, data: list[str]) -> str:
         return ''.join(data)
     
     def print_all(self) -> None:
-        ...
+        LOGGER.info(f"Layer 1: {self.name}")
+        LOGGER.info(f"Destination MAC: {self.dst_mac}")
+        LOGGER.info(f"Source MAC: {self.src_mac}")
 
     def resolve_type(self, hex) -> str | None:
         ...
