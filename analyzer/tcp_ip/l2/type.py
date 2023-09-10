0c45f7ac06972f849184d4f2186809a32c5d8140
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:55:42 +0200

    added get_ip

--- analyzer/tcp_ip/l2/type.py
+++ analyzer/tcp_ip/l2/type.py
@@ -30,3 +30,8 @@ class L2:
 
     def resolve_protocol(self, hex) -> str | None:
         ...
+
+    def get_ip(self, str) -> str | None:
+        string = [str(int(string[i:i+2], 16)) for i in range(0, len(string), 3)]
+        string = self.list_to_str(string).replace(' ', '.')
+        return string
