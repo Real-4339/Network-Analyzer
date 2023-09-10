faa2f93e3d2e4ed8ab12b0eab2ac28a789253778
Real-4339 <qvadym@stuba.sk>
2023-09-11 01:02:53 +0200

    OK

---
+++ analyzer/tcp_ip/l1/raw.py
@@ -0,0 +1,24 @@
+import logging
+
+from type import L1
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('Raw')
+
+
+class Raw(L1):
+    name = "IEEE 802.3 RAW"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+        self.__length = self.list_to_str(hex[12:14]).replace(" ", "")
+    
+    @property
+    def length(self) -> str:
+        return str(int(self.__length, 16))
+    
+    def print_all(self) -> None:
+        super().print_all()
+        LOGGER.info(f"Length: {self.length}")
