896b56a44b238798e01fbd1881ee17ff8e4f0b1a
Real-4339 <qvadym@stuba.sk>
2023-09-11 02:35:27 +0200

    OK

---
+++ analyzer/tcp_ip/l3/igmp.py
@@ -0,0 +1,17 @@
+import logging
+
+from type import L3
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('IGMP')
+
+
+class IGMP(L3):
+    name = "Internet Group Management Protocol"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+    def print_all(self) -> None:
+        super().print_all()
