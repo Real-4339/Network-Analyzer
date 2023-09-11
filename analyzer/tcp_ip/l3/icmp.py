47f489fe675ac554c0b666716cbb3f6b32d0b3fb
Real-4339 <qvadym@stuba.sk>
2023-09-11 02:40:38 +0200

    OK

---
+++ analyzer/tcp_ip/l3/icmp.py
@@ -0,0 +1,28 @@
+import logging
+
+from type import L3
+from lib import ListOfICMP
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('ICMP')
+
+
+class ICMP(L3):
+    name = "ICMP"
+
+    def __init__(self, hex) -> None:
+        super().__init__(self.name, hex)
+
+        self.__type = self.resolve_type(hex[0])
+
+    @property
+    def type(self) -> str:
+        return self.__type
+
+    def print_all(self) -> None:
+        super().print_all()
+        LOGGER.info(f"Type: {self.type}")
+
+    def resolve_type(self, hex: str) -> str:
+        return ListOfICMP.get(hex, 16)
