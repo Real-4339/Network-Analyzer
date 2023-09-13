2a399271785a9fc4d32535e263900f3fef1c296a
Real-4339 <qvadym@stuba.sk>
2023-09-13 12:40:54 +0200

    added init files

---
+++ analyzer/tcp_ip/l2/__init__.py
@@ -0,0 +1,14 @@
+from .arp import ARP
+from .ectp import ECTP
+from .ipv6 import IPv6
+from .ipv4 import IPv4
+from .lldp import LLDP
+
+
+__all__ = [
+    'ARP',
+    'ECTP',
+    'IPv6',
+    'IPv4',
+    'LLDP',
+]
---
+++ analyzer/tcp_ip/l3/__init__.py
@@ -0,0 +1,14 @@
+from .pim import PIM
+from .udp import UDP
+from .tcp import TCP
+from .igmp import IGMP
+from .icmp import ICMP
+
+
+__all__ = [
+    'PIM', 
+    'UDP', 
+    'TCP', 
+    'IGMP', 
+    'ICMP'
+]
