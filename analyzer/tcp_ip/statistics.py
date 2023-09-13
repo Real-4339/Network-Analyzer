489d6dfe5107f21507dfd449dcdede3b995e4394
Real-4339 <qvadym@stuba.sk>
2023-09-13 17:43:02 +0200

    updated

--- analyzer/tcp_ip/statistics.py
+++ analyzer/tcp_ip/statistics.py
@@ -1,3 +1,10 @@
+import logging
+
+
+''' Global variables '''
+LOGGER = logging.getLogger('Statistics')
+
+
 class Statistics:
 
     def __init__(self) -> None:
@@ -6,3 +13,14 @@ class Statistics:
     @property
     def ip_sources(self) -> dict[str, int]:
         return self.__ip_sources
+    
+    def print_statistics(self) -> None:
+        mval = max(self.__ip_sources.values())
+        rad = [[k, v] for k, v in self.__ip_sources.items() if v == mval]
+
+        LOGGER.info(f"Source IPv4 addresses: ")
+        for key, value in self.__ip_sources.items():
+            LOGGER.info(f"{key}: {value} packets")
+
+        for i in rad:
+            LOGGER.info(f"Address/es with the largest number of send packets: {i[0]}. Packets amount: ( {i[1]} )")
