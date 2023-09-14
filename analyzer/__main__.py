fb41af76e43cdbc7314891c32d943921b2b3e073
Real-4339 <qvadym@stuba.sk>
2023-09-14 20:30:47 +0200

    ok

--- analyzer/__main__.py
+++ analyzer/__main__.py
@@ -1,49 +1,70 @@
-from ruamel.yaml import YAML
-from scapy.all import rdpcap
-from module import *
-from argparse import ArgumentParser
+import os
+import logging
 
+# from module import *
+from scapy.all import rdpcap
+
+from tcp_ip.yaml import Basic
+from tcp_ip.packet import Packet
+from argparse import ArgumentParser
+from tcp_ip.lib.lib import parse_file
+from tcp_ip.statistics import Statistics
+from tcp_ip.communications.arp import ARPCom
+from tcp_ip.communications.tftp import TFTPCom
+from tcp_ip.communications.tcp_all import TCPAll
+
+
+''' Global variables '''
+logging.basicConfig(level=logging.NOTSET)
+
+sample = 'eth-2.pcap' # eth-1.pcap eth-3.pcap trace-15.pcap
 
 samples_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'samples'))
 
-packets = rdpcap(samples_path + '/trace-15.pcap')
+packets = rdpcap(samples_path + '/' + sample) 
 
-# def main(): # TF
-    # pcap_filename = 'vzory/trace-7.pcap'
-    # dictoinary['pcap_name'] = "trace-27.pcap"
+stats = Statistics()
 
-    # yaml = YAML()
-    # file = open("PKS.yaml", "w")
-    # yaml.default_flow_style = False
-    # outputs = yaml.dump(dictoinary, file) 
-    # file.close()
 
 def prepinac(protocol):
-    if protocol == 'TFTP':
-        p = TFTP(protocol, packets)
-        p.parsePackets()
-        p.printAll()
-        
-    else:   
-        p = Prepinac(protocol, packets)
-        p.parsePackets()
-        for flags in eval("Prepinac." + p.low).values():
-            print(p.define(flags))
-        p.printAll()
 
-def statistics():
-    ParseFile()
-    stat = Statistics()
-    start_statistics(stat)
+    root = os.path.abspath(os.path.dirname(__file__))
+    parse_file(root)
+
+    if protocol == 'TFTP':
+        p = TFTPCom(packets, stats)
+        p.print_result()
+
+    elif protocol == 'ARP':
+        p = ARPCom(packets, stats)
+        p.print_result()
+        
+    else:
+        p = TCPAll(protocol, packets, stats)
+        p.print_result()
+    #else:
+    # p = Prepinac(protocol, packets)
+    # p.parsePackets()
+    # for flags in eval("Prepinac." + p.low).values():
+    #     print(p.define(flags))
+
+    # p.printAll()
+
+
+def new_statistics():
+
+    root = os.path.abspath(os.path.dirname(__file__))
+    parse_file(root)
+
+    my_packets = []
 
     for index, packet in enumerate(packets):
-        print("<-----Packet----->")
-        p = Packet(packet, index+1)
-        p.printAll()
-        # if index == 10:
-        #     break
+        # print("<-----Packet----->")
+        p = Packet(packet, index+1, stats)
+        my_packets.append(p)
     
-    stat.printStatisctics()
+    basic_yaml = Basic(sample, root, my_packets, stats)
+
 
 if __name__ == '__main__':
 
@@ -61,5 +82,5 @@ if __name__ == '__main__':
         else:
             print("Error")
     else:
-        statistics()
+       new_statistics()
     
