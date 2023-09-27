8a40153e3c15cd0f2de2af6f22a2f1fed63d632c
Real-4339 <qvadym@stuba.sk>
2023-09-27 22:54:09 +0200

    ok

--- analyzer/tcp_ip/l3/icmp.py
+++ analyzer/tcp_ip/l3/icmp.py
@@ -14,10 +14,10 @@ class ICMP(L3):
     def __init__(self, hex) -> None:
         super().__init__(self.name, hex)
 
-        self.resolve_type(hex[0])
-
         self.__identifier: int = None
         self.__sequence_number: int = None
+
+        self.resolve_type(hex[0])
 
     @property
     def type(self) -> str:
@@ -38,9 +38,14 @@ class ICMP(L3):
     def print_all(self) -> None:
         super().print_all()
         LOGGER.info(f"Type: {self.type}")
+        LOGGER.info(f"Identifier: {self.identifier}")
+        LOGGER.info(f"Sequence number: {self.sequence_number}")
 
     def resolve_type(self, hex: str) -> None:
-        self.__type = ListOfICMP.get(hex, 'Information Reply')
+        icmp_hex = int(str(int(hex, 16)), 10)
+        icmp_hex = '0' + str(icmp_hex) if icmp_hex < 10 else str(icmp_hex)
+        
+        self.__type = ListOfICMP.get(icmp_hex, 'Information Reply')
 
         if (self.__type == "Echo reply" or
             self.__type == "Echo request"):
