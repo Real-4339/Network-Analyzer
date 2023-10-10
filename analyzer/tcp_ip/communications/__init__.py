95a7864097973bfe9a486aa4c44c2d1d43399662
Real-4339 <qvadym@stuba.sk>
2023-10-10 16:28:08 +0200

    added abs class

--- analyzer/tcp_ip/communications/__init__.py
+++ analyzer/tcp_ip/communications/__init__.py
@@ -3,9 +3,11 @@ from .arp import ARPCom
 from .icmp import ICMPCom
 from .tcp_all import TCPAll
 from .rip import RIPCom
+from .type import Com
 
 
 __all__ = [
+    'Com',
     'TFTPCom',
     'ARPCom',
     'ICMPCom',
